02f124c3386b851e586f0ebf32925e71407c9554
ChrisBAshton <chrisashtonweb@gmail.com>
2015-04-04 12:00:02 +0100

    maritime collision module inactive by default
    Otherwise we have to make sure we download and install the module when we run Travis tests

--- webapp/modules/config.php
+++ webapp/modules/config.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * This file is a temporary solution to telling the system which modules are 'active' and which are not.
- * In the long term, I'd like to have an admin account and dashboard facility as in WordPress, to install, uninstall, activate and deactivate modules using a nice GUI.
+ * In the long term, I'd like to have an admin account and dashboard facility as in WordPress, to install, uninstall, activate and deactivate modules using a nice GUI. And that information would live in the database.
  */
 $modules = array(
     'other'              => true,
-    'maritime_collision' => true
+    'maritime_collision' => false
 );
 foreach($modules as $moduleKey => $active) {
     if ($active) {
